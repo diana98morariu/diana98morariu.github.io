ee14123544ff7bfc51bb53383c57d6376ec0a604
Diana Morariu <32464376+diana98morariu@users.noreply.github.com>
2020-04-27 10:20:13 +0200

    auto-commit

---
+++ weatherapp/precache-manifest.5d926751a8e43b68f2ebce623f94a7cb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77cdae252a880673b26cf809a2d58097",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "4c4bf5ea589d7557f3b6",
+    "url": "/weatherapp/static/css/2.2efffe0b.chunk.css"
+  },
+  {
+    "revision": "938ecffc06c727bcc628",
+    "url": "/weatherapp/static/css/main.5a08d00c.chunk.css"
+  },
+  {
+    "revision": "4c4bf5ea589d7557f3b6",
+    "url": "/weatherapp/static/js/2.39ea8b5b.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/weatherapp/static/js/2.39ea8b5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "938ecffc06c727bcc628",
+    "url": "/weatherapp/static/js/main.9c737803.chunk.js"
+  },
+  {
+    "revision": "ff873d0a6dca386d57e6",
+    "url": "/weatherapp/static/js/runtime-main.d6b406ef.js"
+  },
+  {
+    "revision": "27b2ec5d55ca2394fd2ff22e6eb89559",
+    "url": "/weatherapp/static/media/Weather-Blog-Image.27b2ec5d.jpg"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/weatherapp/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/weatherapp/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/weatherapp/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/weatherapp/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/weatherapp/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
